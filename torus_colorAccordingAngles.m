5aaa5ba1759941c6b0747b78eb54d934a384658f
ayeletc <41619254+ayeletc@users.noreply.github.com>
2019-11-05 12:20:36 +0200

    Update torus_colorAccordingAngles.m

--- torus_colorAccordingAngles.m
+++ torus_colorAccordingAngles.m
@@ -2,26 +2,82 @@ clear
 close all
 
 
-t1 = rand([1000 1]);
-t2 = rand([1000 1]);
-t3 = rand([1000 1]);
+N = 1000;
+
+t1 = rand([N 1])*2*pi;
+t2 = rand([N 1])*2*pi;
+t3 = rand([N 1])*2*pi;
 
 R = 10;
 r1 = 4;
 r2 = 2;
 
 figure;
-x1 = (R+r1*cos(2*pi*t2)).*cos(2*pi*t1);
-y1 = (R+r1*cos(2*pi*t2)).*sin(2*pi*t1);
-z1 = r1*sin(2*pi*t2);
+x1 = (R+r1*cos(t2)).*cos(t1);
+y1 = (R+r1*cos(t2)).*sin(t1);
+z1 = r1*sin(t2);
 
-x2 = (R+r2*cos(2*pi*t3)).*cos(2*pi*t1);
-y2 = (R+r2*cos(2*pi*t3)).*sin(2*pi*t1);
-z2 = r2*sin(2*pi*t3);
+x2 = (R+r2*cos(t3)).*cos(t1);
+y2 = (R+r2*cos(t3)).*sin(t1);
+z2 = r2*sin(t3);
 
 subplot(2, 1, 1);
-scatter3(x1(:),y1(:),z1(:),1 ,t1*10000, '.')
+scatter3(x1(:),y1(:),z1(:),50 ,t1*10, '.')
 title('s1');
 subplot(2, 1, 2);
-scatter3(x2(:),y2(:),z2(:),1 ,t1*10000, '.')
+scatter3(x2(:),y2(:),z2(:),50 ,t1*10, '.')
 title('s2');
+
+s1 = [x1 y1 z1];
+s2 = [x2 y2 z2];
+
+%% Alternating Diffusion
+
+eps1 = 0.5;
+eps2 = 0.5;
+m = 1;
+d1 = zeros(N);
+d2 = zeros(N);
+
+for i =1:N
+    for j = 1:N
+        d1(i,j) = vecnorm(s1(i,:)-s1(j,:));
+        d2(i,j) = vecnorm(s2(i,:)-s2(j,:));
+    end
+end
+
+W1 = exp(-((d1 .^ 2) / eps1));
+W2 = exp(-((d2 .^ 2) / eps2));
+
+K1 = W1 ./ sum(W1,1);
+K2 = W2 ./ sum(W2,1);
+Ktot = K2*K1;
+ 
+% Ktotm = Ktot ^ m; 
+% for i = 1:N
+%     for j = 1:N
+%         d2m(i,j) = sum((Ktotm(:,i)-Ktotm(:,j)) .^ 2, 1);
+%     end
+% end
+
+%% Diffusion Maps (appendix A)
+
+t = 1;
+
+[V,D] = eig(Ktot);
+D = diag(D^t);
+phi = V(:, 2:4) ./ V(:, 1);
+
+figure;
+subplot(3, 1, 1);
+scatter3(x1(:),y1(:),z1(:), 100 ,V(:,2)*10, '.');
+title('s1');
+subplot(3, 1, 2);
+scatter3(x2(:),y2(:),z2(:), 100 ,V(:,2)*10, '.');
+title('s2');
+subplot(3, 1, 3);
+scatter(cos(t1), sin(t1),100 ,V(:,2)*10, '.');
+title('The common variable');
+xlabel('$$\tilde{x}_{i,1}$$','fontsize',16,'interpreter','latex');
+ylabel('$$\tilde{x}_{i,2}$$','fontsize',16,'interpreter','latex');
+
