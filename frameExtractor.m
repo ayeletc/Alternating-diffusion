ca8b465c697e6f81ba18eedc6b21b656c0c7e556
ayeletc <41619254+ayeletc@users.noreply.github.com>
2019-11-18 11:02:42 +0200

    Update frameExtractor.m
    Function format

--- frameExtractor.m
+++ frameExtractor.m
@@ -1,38 +1,26 @@
-clear
-close all
-
-% We cannot use 30 min video, Matlab camnnot load it
-
-% Data:
-
-% 1)
-% file: '2330_2500.mp4'
-% from day 3 camera 3, minutes 23:30-25:00
-% speakers: 8 & 24
-
-% 2)
-% file: '2000_2200.mp4'
-% from day 3 camera 3, minutes 20:00 - 21:30
-% speakers: 11 & 35
-
-%% Video Reader
-N = 120;  % 50 sec (20 fps)
-% v = VideoReader('2330_2500.mp4');
-v = VideoReader('2000_2200.mp4');
-frames = read(v,[N Inf]);
-numberOfFrames = size(frames, 4)
+function frameMat = frameExtractor(file, N)
+%% Video Load
+v = VideoReader(file);
+frames = read(v,[1 N]);
+numberOfFrames = size(frames, 4);
 
 %% Dealing with specific frame
-i = 10;
-frame_i = frames(:,:,:,i);
-grayFrame_i = rgb2gray(frame_i);
-figure;
-imshow(grayFrame_i);
-totalEnergyInFrame_i = sum(grayFrame_i(:))
+gray_frames=zeros(540,960,1,numberOfFrames);
+for i = 1:N
+    frame = frames(:,:,:,i);
+    gray_frames(:,:,:,i) = rgb2gray(frame);
+end
+%% finding eps
+% 
+% 
+% 
+%% return
+frameMat = squeeze(gray_frames);
 
 
-
-
-
-
-
+if 0
+%% TEST:
+frameMat = frameExtractor('2000_2200.mp4', 300);
+totalEnergyInFrame_i = sum(frameMat(:))
+grayFrame_1 = gray_frames(:,:,:,1);
+end
