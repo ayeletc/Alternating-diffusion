642fbcc38bec1b2c89fb6de68a0b6af62e84a1f8
ayeletc <41619254+ayeletc@users.noreply.github.com>
2019-11-18 11:10:47 +0200

    Update affinityMatrix.m
    Foor loop implementation

--- affinityMatrix.m
+++ affinityMatrix.m
@@ -1,24 +1,17 @@
-function result = affinityMatrix(s, ep)
-% Calculate the affinity matrix of samples vector: s[Nx3], using epislon ep
-    szx = size(s,1);
-    szy = 3; % x, y, z
-    
-    b = s(:);
-    
-    bRep = repmat(b,[1 szy*szx]);
-    bSubt =  bRep-bRep';
-    
-    x = bSubt(1:szx, 1:szx);
-    y = bSubt((1:szx)+szx, (1:szx)+szx);
-    z = bSubt((1:szx)+szx*2, (1:szx)+szx*2);
-    
-    vNorm = vecnorm([x(:) y(:) z(:)],2,2);
-    matNorm = reshape(vNorm, [szx szx]);
-    result = exp(-(matNorm .^ 2) / ep);
+function K = affinityMatrix(samples,ep)
+% Calculate the affinity matrix of the frames matrix: s[szx x szy x N], using epislon ep
+% szx x szy is the size of a single frame
 
-if 0
-%% Test:
-    a = [10,20, 30, 40; 1, 2, 3, 4; 100, 200, 300, 400]';
-    a = a ./ 100;
-    affinityMatrix(a, ep)
-end
+szx=size(samples,1); %x of frame
+szy=size(samples,2); %y of frame
+szn=size(samples,3); %number of frames
+    
+b = reshape(samples,[szx*szy szn]);
+
+ for i = 1:szn
+     for j = 1: szn
+         matNorm(i,j)= vecnorm(b(i,:)-b(j,:));
+     end
+ end
+ 
+K = (exp(-(matNorm .^ 2) / ep));
