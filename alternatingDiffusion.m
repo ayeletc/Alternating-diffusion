b4432024f9b59a02417cc4bb4b19585aa780b5c7
ayeletc <41619254+ayeletc@users.noreply.github.com>
2019-12-09 10:05:46 +0200

    Update alternatingDiffusion.m
    options for symmetric and antisymmetric kernels

--- alternatingDiffusion.m
+++ alternatingDiffusion.m
@@ -1,12 +1,14 @@
 %% Alternating diffusion
-function result = alternatingDiffusion(frameMat,ep1,accSample,ep2)
+function [K_sym, K_antisym, K_gen] = alternatingDiffusion(frameMat,ep1,accSample,ep2)
 % get matrix of frames with its epsilon and accelerators samples with its epsilon
-% return K - the alternating diffusion kernel 
+% return [K_sym, K_antisym, K_gen] - the alternating diffusion kernels
 
-W1 = affinityMatrix(frameMat,ep1);
+W1 = affinityMatrixFrames(frameMat,ep1);
 W2 = affinityMatrixAcc(accSample,ep2);
 
 K1 = W1 ./ sum(W1,1);
 K2 = W2 ./ sum(W2,1);
 
-result = K2 * K1;
+K_sym = K1 * K2.' + K2 * K1.';
+K_antisym = K1 * K2.' - K2 * K1.';
+K_gen = K2 * K1;
