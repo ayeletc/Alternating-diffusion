a60d84099dc5dbbcfab8d641af46c5c4230ff260
ayeletc <41619254+ayeletc@users.noreply.github.com>
2019-12-09 15:51:05 +0200

    Update plotEigenVectorsColoredByLabels.m
    option to choose which eigenvectors to plot

--- plotEigenVectorsColoredByLabels.m
+++ plotEigenVectorsColoredByLabels.m
@@ -1,12 +1,12 @@
-function result = plotEigenVectorsColoredByLabels(id, speakerLabels, eigenVectors)
-speakerLabels = load(['Data' filesep 'CSVs' filesep 'speaker' num2str(id) '.csv']);
+function result = plotEigenVectorsColoredByLabels(id, speakerLabels, eigenVectors, V1id, V2id)
+speakerLabels = load(['Data' filesep 'CSVs' filesep 'speaker29.csv']);
 speakerLabels = speakerLabels(:);
 
 figure;
-scatter(eigenVectors(:,2),eigenVectors(:,3),500, speakerLabels, '.');
-title(['V_2, V_3 colored by speaker_{' num2str(id) '} speaking labels']);
-xlabel('$$V_2$$','fontsize',16,'interpreter','latex');
-ylabel('$$V_3$$','fontsize',16,'interpreter','latex');
+scatter(eigenVectors(:,V1id),eigenVectors(:,V2id),500, speakerLabels, '.');
+title(['V_' num2str(V1id) ', V_' num2str(V2id) ' colored by speaker_{' num2str(id) '} speaking labels']);
+xlabel(['$$V_' num2str(V1id) '$$'],'fontsize',16,'interpreter','latex');
+ylabel(['$$V_' num2str(V2id) '$$'],'fontsize',16,'interpreter','latex');
 xlim([-0.2 0.2])
 ylim([-0.2 0.2])
 grid on
