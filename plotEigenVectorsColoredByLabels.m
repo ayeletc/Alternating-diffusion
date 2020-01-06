9f876703ed3645e8ae1c2e79ff56fb33d8ae7033
ayeletc <41619254+ayeletc@users.noreply.github.com>
2020-01-06 09:48:15 +0200

    Update plotEigenVectorsColoredByLabels.m

--- plotEigenVectorsColoredByLabels.m
+++ plotEigenVectorsColoredByLabels.m
@@ -3,11 +3,12 @@ speakerLabels = load(['Data' filesep 'CSVs' filesep 'speaker' num2str(id) '.csv'
 speakerLabels = speakerLabels(:);
 
 figure;
-scatter(eigenVectors(:,V1id),eigenVectors(:,V2id),500, speakerLabels, '.');
+hold on;
+scatter(eigenVectors(:,V1id),eigenVectors(:,V2id),100, speakerLabels, '.');
 title(['V_' num2str(V1id) ', V_' num2str(V2id) ' colored by speaker_{' num2str(id) '} speaking labels']);
 xlabel(['$$V_' num2str(V1id) '$$'],'fontsize',16,'interpreter','latex');
 ylabel(['$$V_' num2str(V2id) '$$'],'fontsize',16,'interpreter','latex');
-xlim([-0.2 0.2])
-ylim([-0.2 0.2])
 grid on
+colormap(cool(2));
+colorbar;
 end
